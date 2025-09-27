42a9ae07fcf714f995725b483916b2b89903b6b2
HussainALAradi5 <hussainaradi.ha@gmail.com>
2025-09-28 02:28:53 +0300

    add major changes to the user model

--- server/src/main/java/com/server/server/Configuration/SecurityConfiguration.java
+++ server/src/main/java/com/server/server/Configuration/SecurityConfiguration.java
@@ -2,14 +2,40 @@ package com.server.server.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.GET, "/").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+            .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults()) 
+            .build();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- server/src/main/java/com/server/server/Repositories/UserRepository.java
+++ server/src/main/java/com/server/server/Repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.server.server.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.server.server.Models.User;
 
 @Repository
 public interface  UserRepository  extends JpaRepository<User, Long> {
-  
+  Optional<User> findByUserName(String userName);
 }
